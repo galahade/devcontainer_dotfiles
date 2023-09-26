0677c3beb01428b5f8c10cafa27248b6f77f0d50
Young Yang <galahade@sina.com>
2023-09-26 11:17:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,25 @@
 
 #cp ./.gitmessage ~
 #cp ./.gitconfig ~
+# Install MesloLGS Nerd Font 
+if [ "$(fc-list : family | grep 'MesloLGS Nerd Font')" == "" ]; then
+    mkdir fonts && cd fonts || exit
+    curl -OL https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Meslo.tar.xz
+    tar -xf Meslo.tar.xz -C ~/.local/share/fonts      
+    cd .. && rm -rf fonts
+else
+    echo 'MesloLGS Nerd Font has been installed'
+fi
+
+sh -c "$(cat ./zsh-in-docker.sh)" -- \
+    -p git \
+    -p https://github.com/zsh-users/zsh-autosuggestions \
+    -p https://github.com/zsh-users/zsh-completions \
+    -p https://github.com/zsh-users/zsh-history-substring-search \
+    -p https://github.com/zsh-users/zsh-syntax-highlighting \
+    -p 'history-substring-search' \
 
 # replace default .zshrc fi
-rm -f ~/.zshrc
 cp ./.zshrc ~
 # use pre-configured p10k
 cp ./.p10k.zsh ~
